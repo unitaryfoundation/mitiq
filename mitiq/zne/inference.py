2ea979cb84f9021cb3d0014ce38ffbee24f95b0c
crnlchez1234-afk <crnlchez1234@gmail.com>
2026-06-03 19:06:34 -0700

    Fix ruff import ordering error

--- mitiq/zne/inference.py
+++ mitiq/zne/inference.py
@@ -20,7 +20,9 @@ from matplotlib.figure import Figure
 try:
     from numpy.exceptions import RankWarning
 except ImportError:
-    from numpy.linalg import LinAlgError as RankWarning  # type: ignore[assignment]
+    from numpy.linalg import (
+        LinAlgError as RankWarning,  # type: ignore[assignment]
+    )
 from scipy.optimize import OptimizeWarning, curve_fit
 
 from mitiq import QPROGRAM, QuantumResult
